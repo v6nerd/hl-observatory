74e45bf90739e28559ca08dd963f38543545cc54
Roy Kokkelkoren <roy.kokkelkoren@gmail.com>
2016-02-23 01:33:26 +0100

    Added creation of signatures for results

--- observatory.py
+++ observatory.py
@@ -1,4 +1,5 @@
 import os
+import rsa
 import api
 import sys
 import json
@@ -53,6 +54,17 @@ def start():
     t.start()
     return t
 
+def read_pub_key():
+    with open('pubkey.pem','rb') as f:
+        keydata = f.read()
+
+    return rsa.PublicKey.load_pkcs1(keydata)
+
+def write_to_file(location, data, mode='w'):
+    with open(location, mode) as f:
+        f.write(data)
+    log.debug("Written to: %s",location)
+
 def run(name):
     results = dict()
 
@@ -63,17 +75,23 @@ def run(name):
             dst = line.strip()
             results[dst] = analyse.analyse_domain(dst)
 
-    json_io = json.dumps(results,separators=(',', ':')) #remove whitespaces
-    log.info("[Results] %s",json_io)
+    json_io = json.dumps(results, separators=(',', ':'), sort_keys=True) #remove whitespaces
+    sha256sum = hashlib.sha256(json_io.encode('utf-8')).hexdigest()
+    log.info("Results: [%s] %s",sha256sum,json_io)
 
     now = datetime.datetime.now()
-    str_now = now.strftime("%H%M_%d%m%y")
+    str_now = now.strftime("%H%M%d%m%y")
     file_name = "{0}_{1}.json".format(name,str_now)
 
-    with open('results/'+file_name,'w') as f:
-        f.write(json_io)
+    # Write results
+    loc = 'results/{0}'.format(file_name)
+    write_to_file(loc, json_io)
 
-    log.info("Written results to file: %s",file_name)
+    # Write encrypted signature
+    loc = 'results/{0}.asc'.format(file_name)
+    pubkey = read_pub_key()
+    enc_sign = rsa.encrypt(sha256sum.encode('utf-8'), pubkey)
+    write_to_file(loc, enc_sign,'wb')
 
 def main():
 

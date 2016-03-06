a7555462371358a17a47c8e11f48b9e6713da762
v6nerd <v6nerd@github.com>
2016-03-06 19:25:45 +0100

    new

---
+++ get_top500_alexa.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+for item in {0..19};do curl -o alexa_global.$item "http://www.alexa.com/topsites/global;$item" && grep "\<a href\=\"\/siteinfo\/" alexa_global.$item | awk -F">" '{print $7}' | cut -d"<" -f1 >> alexa_top500.list;done

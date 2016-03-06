2abc5d9e0ecc2a6b80fefb2cbe5759fd6af7d628
v6nerd <v6nerd@github.com>
2016-03-06 19:32:52 +0100

    updated

--- get_top500_alexa.sh
+++ get_top500_alexa.sh
@@ -1,5 +1,4 @@
 #Retreives the top 500 Alexa sites put it in single .list file
 
 #!/bin/bash
-for item in {0..19};do curl -o alexa_global.$item "http://www.alexa.com/topsites/global;$item" && grep "\<a href\=\"\/siteinfo\/" alexa_global.$item | awk -F">" '{print $7}' | cut -d"<" -f1 >> alexa_top500.list;done
-
+for item in {0..19};do curl -o alexa_global.$item "http://www.alexa.com/topsites/global;$item" && grep "\<a href\=\"\/siteinfo\/" alexa_global.$item | awk -F">" '{print $7}' | cut -d"<" -f1 >> alexa_top500.list && rm -r alexa_global.$item;done

d71c12ba91c703b4632bfa5612341d1e2a1eccce
v6nerd <v6nerd@github.com>
2016-03-10 19:22:49 +0100

    replaced with hl_setup.sh

--- pre-docker.sh
+++
@@ -1,5 +0,0 @@
-#pre-docker setup scrip over SSH - tested
-apt-get update && apt-get install -y docker.io
-docker pull v6nerd/hl-observatory:v1 && docker run -i v6nerd/hl-observatory:v1 /bin/bash
-cat ./results/*.json
-exit

d60f79ce9a7fb252ee035a26f2ccc48a05fa40c7
v6nerd <v6nerd@github.com>
2016-03-10 19:04:43 +0100

    Permenant ENV Variable SET

--- get_geoinfo.sh
+++ get_geoinfo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #This Script retrives the geo ip information and exports detailed geoip.info file and sets hostinfo as an enviorment variable
 geoip=$(curl -s api.ipify.org)
-curl -s ipinfo.io/$geoip > geoip.info && export hostinfo=$(cat geoip.info | sed -e 's/[{}:"]/''/g' | grep -E "ip|hostname|country" | awk -F" " '{print $2}' | tr -d "\r\n" | tr ',' '_' | sed s/.$//g)
+curl -s ipinfo.io/$geoip > geoip.info && hostinfo=$(cat geoip.info | sed -e 's/[{}:"]/''/g' | grep -E "ip|hostname|country" | awk -F" " '{print $2}' | tr -d "\r\n" | tr ',' '_' | sed s/.$//g)
+echo "HOSTINFO="$hostinfo >> ~/.bashrc

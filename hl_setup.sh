bd5c08ed0179cfbffd1a41092566b9429a749e1a
v6nerd <v6nerd@github.com>
2016-03-10 23:30:11 +0100

    instance launch foxes

--- hl_setup.sh
+++ hl_setup.sh
@@ -2,7 +2,6 @@
 echo SETUP STARTED at  $(date +%d-%m-%y' '%H:%M:%S) >> /var/log/hl-observatory.setup
 apt-get update && apt-get install -y docker.io >> /var/log/hl-observatory.setup
 docker pull v6nerd/hl-observatory:latest && echo SETUP ENDED at $(date +%d-%m-%y' '%H:%M:%S)  >> /var/log/hl-observatory.setup
-docker run --name default -i v6nerd/hl-observatory:latest /hl-observatory/get_geoinfo.sh
-docker start -i default
-python3 /hl-observatory/observatory.py -n $HOSTINFO -t targets.lst
-cat ./results/*.json && exit
+docker run --name default -i v6nerd/hl-observatory:latest /bin/bash 
+./get_geoinfo.sh && python3 /hl-observatory/observatory.py -n $(cat hostinfo) -t targets.lst && cat ./results/*.json 
+exit

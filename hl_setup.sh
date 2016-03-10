23755792a35bad4391a63f13034bd2ca19d6cde6
v6nerd <v6nerd@github.com>
2016-03-10 19:55:35 +0100

    added:get_geoinfo at RUNtiME

--- hl_setup.sh
+++ hl_setup.sh
@@ -3,5 +3,6 @@ echo SETUP STARTED at  $(date +%d-%m-%y' '%H:%M:%S) >> /var/log/hl-observatory.s
 apt-get update && apt-get install -y docker.io >> /var/log/hl-observatory.setup
 docker pull v6nerd/hl-observatory:latest && echo SETUP ENDED at $(date +%d-%m-%y' '%H:%M:%S)  >> /var/log/hl-observatory.setup
 docker run -i v6nerd/hl-observatory:latest /bin/bash
+./get_geoinfo.sh
 python3 /hl-observatory/observatory.py -n $HOSTINFO -t targets.lst
 cat ./results/*.json && exit

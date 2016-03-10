e057895e38516a7bc20b42b28eb32d0d5c542a13
v6nerd <v6nerd@github.com>
2016-03-10 19:40:15 +0100

    Added SETUP time log

--- hl_setup.sh
+++ hl_setup.sh
@@ -1,5 +1,7 @@
-#Docker Instance setup script over SSH - tested
-apt-get update && apt-get install -y docker.io
-docker pull v6nerd/hl-observatory:latest && docker run -i v6nerd/hl-observatory:latest /bin/bash
+#HL-Observatory SETUP script over SSH - Tested (single)
+echo SETUP STARTED at  $(date +%d-%m-%y' '%H:%M:%S) >> /var/log/hl-observatory.setup
+apt-get update && apt-get install -y docker.io >> /var/log/hl-observatory.setup
+docker pull v6nerd/hl-observatory:latest && echo SETUP ENDED at $(date +%d-%m-%y' '%H:%M:%S)  >> /var/log/hl-observatory.setup
+docker run -i v6nerd/hl-observatory:latest /bin/bash
 python3 /hl-observatory/observatory.py -n $HOSTINFO -t targets.lst
 cat ./results/*.json && exit

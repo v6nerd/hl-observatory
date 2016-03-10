8e17f4a67b82e12584f7ef7c0f1b386b1c67c504
v6nerd <v6nerd@github.com>
2016-03-10 19:24:14 +0100

    Test RUN Command Added

--- hl_setup.sh
+++ hl_setup.sh
@@ -1,4 +1,5 @@
 #Docker Instance setup script over SSH - tested
 apt-get update && apt-get install -y docker.io
 docker pull v6nerd/hl-observatory:latest && docker run -i v6nerd/hl-observatory:latest /bin/bash
+python3 /hl-observatory/observatory.py -n $HOSTINFO -t targets.lst
 cat ./results/*.json && exit

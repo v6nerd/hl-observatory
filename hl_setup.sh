56160cd73150111e0d92dfe59f3aecfac679c78c
v6nerd <v6nerd@github.com>
2016-03-11 13:51:27 +0100

    Instance Launch Fixes

--- hl_setup.sh
+++ hl_setup.sh
@@ -3,5 +3,5 @@ echo SETUP STARTED at  $(date +%d-%m-%y' '%H:%M:%S) >> /var/log/hl-observatory.s
 apt-get update && apt-get install -y docker.io >> /var/log/hl-observatory.setup
 docker pull v6nerd/hl-observatory:latest && echo SETUP ENDED at $(date +%d-%m-%y' '%H:%M:%S)  >> /var/log/hl-observatory.setup
 docker run --name default -i v6nerd/hl-observatory:latest /bin/bash
-./get_geoinfo.sh && python3 /hl-observatory/observatory.py -n $(cat ./hostinfo) -t targets.lst && cat ./results/*.json 
+./get_geoinfo.sh && python3 observatory.py -n $(cat hostinfo) -t targets.lst && cat ./results/*.json
 exit

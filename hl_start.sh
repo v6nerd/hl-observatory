948308ca0f6c0ca2e5176e25f90965616159e8ad
v6nerd <v6nerd@github.com>
2016-03-17 02:44:48 +0100

    updated with pre alexa run test

--- hl_start.sh
+++ hl_start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ./get_geoinfo.sh && cat hostinfo
 python3 observatory.py -n test_$(cat hostinfo) -t targets.lst
+if [ $(cat ./results/*test*.json | python3 -m json.tool | wc -l) -gt 10 ];then python3 observatory.py -n $(cat hostinfo) -t alexa_*.list;else echo "Test Failed";fi
+

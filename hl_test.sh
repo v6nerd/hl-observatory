9d80a6790fed8d9ca5d78cca5b93f13bd298b2b1
v6nerd <v6nerd@github.com>
2016-03-23 19:21:19 +0100

    results location seek

--- hl_test.sh
+++ hl_test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 LOGFILE="/var/log/hl-observatory.setup"
-EXECPATH="/hl-observatroy"
+EXECPATH=$(PWD)
 FMTDATE=$(date +%d-%m-%y' '%H:%M:%S)
 ./get_geoinfo.sh
 python3 observatory.py -n test_$(cat hostinfo) -t targets.lst
-if [ $(cat ./results/*test*.json | python3 -m json.tool | wc -l) -gt 10 ];then echo "$FMTDATE - Initial Test Successful!" >> $LOGFILE && $EXECPATH/hl_exec.sh;else echo "$FMTDATE - Initial Test Failed" >> $LOGFILE;fi
+if [ $(cat $EXECPATH/results/test*.json | python3 -m json.tool | wc -l) -gt 10 ];then echo "$FMTDATE - Initial Test Successful!" >> $LOGFILE && $EXECPATH/hl_exec.sh;else echo "$FMTDATE - Initial Test Failed" >> $LOGFILE;fi
+

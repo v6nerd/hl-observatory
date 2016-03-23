3e124ebf71908a566744f882cbf1ae90a39ec774
v6nerd <v6nerd@github.com>
2016-03-23 20:33:49 +0100

    typo correction

--- hl_exec.sh
+++ hl_exec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-EXECPATH="/hl-observatory"
+EXECPATH=$(pwd)
 
 $EXECPATH/get_geoinfo.sh
 python3 observatory.py -n $(cat $EXECPATH/hostinfo) -t alexa_*.list && echo "COMPLETED Successfuly!"
--- hl_test.sh
+++ hl_test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 LOGFILE="/var/log/hl-observatory.setup"
-EXECPATH=$(PWD)
+EXECPATH=$(pwd)
 FMTDATE=$(date +%d-%m-%y' '%H:%M:%S)
 ./get_geoinfo.sh
 python3 observatory.py -n test_$(cat hostinfo) -t targets.lst
 if [ $(cat $EXECPATH/results/test*.json | python3 -m json.tool | wc -l) -gt 10 ];then echo "$FMTDATE - Initial Test Successful!" >> $LOGFILE && $EXECPATH/hl_exec.sh;else echo "$FMTDATE - Initial Test Failed" >> $LOGFILE;fi
-
